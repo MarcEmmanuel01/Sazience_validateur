f73cba289f2196336912f1932508b5dc0c32c234
MarcEmmanuel01 <emmanuelkassi07@gmail.com>
2025-11-17 16:43:25 +0000

    17/11 2

--- SOURCES/CREATE_VIEW.sql
+++ SOURCES/CREATE_VIEW.sql
@@ -32,3 +32,27 @@ WHERE ENAFLG_0 = 2;
 
 
 
+
+
+CREATE VIEW source_saisies_pieces AS
+SELECT 
+    acc.Comptes_comptables,
+    bpr.Code_tiers, 
+    jou.Code_Journaux,
+    cce.Axe_analytiques
+FROM 
+    (SELECT ACC_0 AS Comptes_comptables, ROW_NUMBER() OVER (ORDER BY ACC_0) as rn 
+     FROM [x3data].[SAZIENCE].[GACCOUNT] 
+     WHERE ENAFLG_0 = 2) acc
+FULL OUTER JOIN 
+    (SELECT BPRNUM_0 AS Code_tiers, ROW_NUMBER() OVER (ORDER BY BPRNUM_0) as rn 
+     FROM [x3data].[SAZIENCE].[BPARTNER] 
+     WHERE ENAFLG_0 = 2) bpr ON acc.rn = bpr.rn
+FULL OUTER JOIN 
+    (SELECT JOU_0 AS Code_Journaux, ROW_NUMBER() OVER (ORDER BY JOU_0) as rn 
+     FROM [x3data].[SAZIENCE].[GJOURNAL] 
+     WHERE ENAFLG_0 = 2) jou ON COALESCE(acc.rn, bpr.rn) = jou.rn
+FULL OUTER JOIN 
+    (SELECT CCE_0 AS Axe_analytiques, ROW_NUMBER() OVER (ORDER BY CCE_0) as rn 
+     FROM [x3data].[SAZIENCE].[CACCE] 
+     WHERE ENAFLG_0 = 2) cce ON COALESCE(acc.rn, bpr.rn, jou.rn) = cce.rn;
